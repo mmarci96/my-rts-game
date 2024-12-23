8b15d8e5c50dcb9575592d96fc9e3b6398615d33
mmarci96 <sarosdimarci@gmail.com>
2024-12-23 02:07:49 +0100

    edited client so it recognizing when running in vite dev server or not

--- client/src/game/ui/AssetManager.js
+++ client/src/game/ui/AssetManager.js
@@ -1,6 +1,7 @@
 class AssetManager {
     constructor() {
         this.images = new Map();
+        this.baseURL = import.meta.env.VITE_LOCAL ? 'http://localhost:5173' : 'http://localhost'; 
     }
 
     /**
@@ -20,7 +21,7 @@ class AssetManager {
             console.error(`Failed to load image: ${src}`, err);
             reject(err);
             };
-            img.src = src;
+            img.src = `${this.baseURL}${src}`;
         });
     }
 
@@ -40,61 +41,61 @@ class AssetManager {
         try {
             await Promise.all([
                 // Warriors
-                this.loadImage('warrior_blue', 'http://localhost/assets/Warrior/Blue/Warrior_Blue.png'),
-                this.loadImage('warrior_purple', 'http://localhost/assets/Warrior/Purple/Warrior_Purple.png'),
-                this.loadImage('warrior_red', 'http://localhost/assets/Warrior/Red/Warrior_Red.png'),
-                this.loadImage('warrior_yellow', 'http://localhost/assets/Warrior/Yellow/Warrior_Yellow.png'),
+                this.loadImage('warrior_blue', '/assets/Warrior/Blue/Warrior_Blue.png'),
+                this.loadImage('warrior_purple', '/assets/Warrior/Purple/Warrior_Purple.png'),
+                this.loadImage('warrior_red', '/assets/Warrior/Red/Warrior_Red.png'),
+                this.loadImage('warrior_yellow', '/assets/Warrior/Yellow/Warrior_Yellow.png'),
 
                 // Pawns
-                this.loadImage('pawn_blue', 'http://localhost/assets/Pawn/Blue/Pawn_Blue.png'),
-                this.loadImage('pawn_purple', 'http://localhost/assets/Pawn/Purple/Pawn_Purple.png'),
-                this.loadImage('pawn_red', 'http://localhost/assets/Pawn/Red/Pawn_Red.png'),
-                this.loadImage('pawn_yellow', 'http://localhost/assets/Pawn/Yellow/Pawn_Yellow.png'),
+                this.loadImage('pawn_blue', '/assets/Pawn/Blue/Pawn_Blue.png'),
+                this.loadImage('pawn_purple', '/assets/Pawn/Purple/Pawn_Purple.png'),
+                this.loadImage('pawn_red', '/assets/Pawn/Red/Pawn_Red.png'),
+                this.loadImage('pawn_yellow', '/assets/Pawn/Yellow/Pawn_Yellow.png'),
 
                 // Archers + Bow
-                this.loadImage('archer_blue', 'http://localhost/assets/Archer/Blue/Archer_Blue.png'),
-                this.loadImage('archer_purple', 'http://localhost/assets/Archer/Purple/Archer_Purlple.png'),
-                this.loadImage('archer_red', 'http://localhost/assets/Archer/Red/Archer_Red.png'),
-                this.loadImage('archer_yellow', 'http://localhost/assets/Archer/Yellow/Archer_Yellow.png'),
-                this.loadImage('archer_bow_blue', 'http://localhost/assets/Archer/Archer%20+%20Bow/Archer_Bow_Blue.png'),
-                this.loadImage('archer_bow_purple', 'http://localhost/assets/Archer/Archer%20+%20Bow/Archer_Bow_Purple.png'),
-                this.loadImage('archer_bow_red', 'http://localhost/assets/Archer/Archer%20+%20Bow/Archer_Bow_Red.png'),
-                this.loadImage('archer_bow_yellow', 'http://localhost/assets/Archer/Archer%20+%20Bow/Archer_Bow_Yellow.png'),
+                this.loadImage('archer_blue', '/assets/Archer/Blue/Archer_Blue.png'),
+                this.loadImage('archer_purple', '/assets/Archer/Purple/Archer_Purlple.png'),
+                this.loadImage('archer_red', '/assets/Archer/Red/Archer_Red.png'),
+                this.loadImage('archer_yellow', '/assets/Archer/Yellow/Archer_Yellow.png'),
+                this.loadImage('archer_bow_blue', '/assets/Archer/Archer%20+%20Bow/Archer_Bow_Blue.png'),
+                this.loadImage('archer_bow_purple', '/assets/Archer/Archer%20+%20Bow/Archer_Bow_Purple.png'),
+                this.loadImage('archer_bow_red', '/assets/Archer/Archer%20+%20Bow/Archer_Bow_Red.png'),
+                this.loadImage('archer_bow_yellow', '/assets/Archer/Archer%20+%20Bow/Archer_Bow_Yellow.png'),
 
                 // Arrow
-                this.loadImage('arrow', 'http://localhost/assets/Archer/Arrow/Arrow.png'),
+                this.loadImage('arrow', '/assets/Archer/Arrow/Arrow.png'),
 
                 // Dead
-                this.loadImage('dead', 'http://localhost/assets/Dead/Dead.png'),
+                this.loadImage('dead', '/assets/Dead/Dead.png'),
 
                 // Tileset
-                this.loadImage('directional_sign', 'http://localhost/assets/TileSet/Tiles/directional_sign.png'),
-                this.loadImage('dirt', 'http://localhost/assets/TileSet/Tiles/dirt.png'),
-                this.loadImage('fence', 'http://localhost/assets/TileSet/Tiles/fence.png'),
-                this.loadImage('fence10', 'http://localhost/assets/TileSet/Tiles/fence10.png'),
-                this.loadImage('fence11', 'http://localhost/assets/TileSet/Tiles/fence11.png'),
-                this.loadImage('fence2', 'http://localhost/assets/TileSet/Tiles/fence2.png'),
-                this.loadImage('fence3', 'http://localhost/assets/TileSet/Tiles/fence3.png'),
-                this.loadImage('fence4', 'http://localhost/assets/TileSet/Tiles/fence4.png'),
-                this.loadImage('fence5', 'http://localhost/assets/TileSet/Tiles/fence5.png'),
-                this.loadImage('fence6', 'http://localhost/assets/TileSet/Tiles/fence6.png'),
-                this.loadImage('fence7', 'http://localhost/assets/TileSet/Tiles/fence7.png'),
-                this.loadImage('fence8', 'http://localhost/assets/TileSet/Tiles/fence8.png'),
-                this.loadImage('fence9', 'http://localhost/assets/TileSet/Tiles/fence9.png'),
-                this.loadImage('grass1', 'http://localhost/assets/TileSet/Tiles/grass1.png'),
-                this.loadImage('lantern1', 'http://localhost/assets/TileSet/Tiles/lantern1.png'),
-                this.loadImage('lantern2', 'http://localhost/assets/TileSet/Tiles/lantern2.png'),
-                this.loadImage('lantern3', 'http://localhost/assets/TileSet/Tiles/lantern3.png'),
-                this.loadImage('lantern4', 'http://localhost/assets/TileSet/Tiles/lantern4.png'),
-                this.loadImage('pavement1', 'http://localhost/assets/TileSet/Tiles/pavement1.png'),
-                this.loadImage('pavement2', 'http://localhost/assets/TileSet/Tiles/pavement2.png'),
-                this.loadImage('scarecrow', 'http://localhost/assets/TileSet/Tiles/scarecrow.png'),
-                this.loadImage('stone', 'http://localhost/assets/TileSet/Tiles/stone.png'),
-                this.loadImage('stone2', 'http://localhost/assets/TileSet/Tiles/stone2.png'),
-                this.loadImage('stone_slab', 'http://localhost/assets/TileSet/Tiles/stoneSlab.png'),
-                this.loadImage('stone_stair', 'http://localhost/assets/TileSet/Tiles/stoneStair.png'),
-                this.loadImage('water1', 'http://localhost/assets/TileSet/Tiles/waterV1.png'),
-                this.loadImage('wheat', 'http://localhost/assets/TileSet/Tiles/wheat.png'),
+                this.loadImage('directional_sign', '/assets/TileSet/Tiles/directional_sign.png'),
+                this.loadImage('dirt', '/assets/TileSet/Tiles/dirt.png'),
+                this.loadImage('fence', '/assets/TileSet/Tiles/fence.png'),
+                this.loadImage('fence10', '/assets/TileSet/Tiles/fence10.png'),
+                this.loadImage('fence11', '/assets/TileSet/Tiles/fence11.png'),
+                this.loadImage('fence2', '/assets/TileSet/Tiles/fence2.png'),
+                this.loadImage('fence3', '/assets/TileSet/Tiles/fence3.png'),
+                this.loadImage('fence4', '/assets/TileSet/Tiles/fence4.png'),
+                this.loadImage('fence5', '/assets/TileSet/Tiles/fence5.png'),
+                this.loadImage('fence6', '/assets/TileSet/Tiles/fence6.png'),
+                this.loadImage('fence7', '/assets/TileSet/Tiles/fence7.png'),
+                this.loadImage('fence8', '/assets/TileSet/Tiles/fence8.png'),
+                this.loadImage('fence9', '/assets/TileSet/Tiles/fence9.png'),
+                this.loadImage('grass1', '/assets/TileSet/Tiles/grass1.png'),
+                this.loadImage('lantern1', '/assets/TileSet/Tiles/lantern1.png'),
+                this.loadImage('lantern2', '/assets/TileSet/Tiles/lantern2.png'),
+                this.loadImage('lantern3', '/assets/TileSet/Tiles/lantern3.png'),
+                this.loadImage('lantern4', '/assets/TileSet/Tiles/lantern4.png'),
+                this.loadImage('pavement1', '/assets/TileSet/Tiles/pavement1.png'),
+                this.loadImage('pavement2', '/assets/TileSet/Tiles/pavement2.png'),
+                this.loadImage('scarecrow', '/assets/TileSet/Tiles/scarecrow.png'),
+                this.loadImage('stone', '/assets/TileSet/Tiles/stone.png'),
+                this.loadImage('stone2', '/assets/TileSet/Tiles/stone2.png'),
+                this.loadImage('stone_slab', '/assets/TileSet/Tiles/stoneSlab.png'),
+                this.loadImage('stone_stair', '/assets/TileSet/Tiles/stoneStair.png'),
+                this.loadImage('water1', '/assets/TileSet/Tiles/waterV1.png'),
+                this.loadImage('wheat', '/assets/TileSet/Tiles/wheat.png'),
             ]);
             return this;
         } catch (err) {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: 'http://localhost:8080', // Backend WebSocket server
-        ws: true, // Enable WebSocket proxying
-        changeOrigin: true, // Adjust the origin header for cross-origin requests
-      },
-      '/api': {
-        target: 'http://localhost:8080', // Backend API server
-        changeOrigin: true,
-      },
+    server: {
+        proxy: {
+            '/socket.io': {
+                target: 'http://localhost:8080', // Backend WebSocket server
+                ws: true, // Enable WebSocket proxying
+                changeOrigin: true, // Adjust the origin header for cross-origin requests
+            },
+            '/api': {
+                target: 'http://localhost:8080', // Backend API server
+                changeOrigin: true,
+            },
+        },
     },
-  },
+    define: {
+        'process.env.VITE_LOCAL': JSON.stringify(process.env.NODE_ENV === 'development'),
+    },
 });
