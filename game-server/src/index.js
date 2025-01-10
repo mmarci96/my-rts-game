1e2841249a79db451dea6e9106f5d5763acf4c34
mmarci96 <sarosdimarci@gmail.com>
2025-01-10 11:05:48 +0100

    we

--- game-server/src/index.js
+++ game-server/src/index.js
@@ -27,7 +27,7 @@ const main = async () => {
     .then(() => console.log("Connected to db: ", MONGO_URI))
     .catch(err => console.error(err))
 
-    server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+    server.listen(PORT,'0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`))
 }
 
 
