519c35e8ab8cec81b02b4a423bf1cf10724b1a98
mmarci96 <sarosdimarci@gmail.com>
2025-01-21 10:46:41 +0100

    parsed the kubeconfigs through chat-gpt and replaced envs and repository names with terraform vars

--- terraform-configs/k8s.tf
+++ terraform-configs/k8s.tf
@@ -69,4 +69,169 @@ resource "kubernetes_deployment" "rts_game_flask_app" {
     }
   }
 }
+resource "kubernetes_service" "rts_game_flask_app" {
+  metadata {
+    name      = "rts-game-flask-app"
+    namespace = "default"
+  }
 
+  spec {
+    selector = {
+      app = "rts-game-flask-app"
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 5000
+      target_port = 5000
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_deployment" "rts_game_client" {
+  metadata {
+    name      = "rts-game-client"
+    namespace = "default"
+    labels = {
+      app = "rts-game-client"
+    }
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "rts-game-client"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "rts-game-client"
+        }
+      }
+
+      spec {
+        container {
+          name  = "rts-game-client"
+          image = "${aws_ecr_repository.repositories["rts-game-client-ecr"].repository_url}:latest"
+
+          resources {
+            requests = {
+              cpu    = "250m"
+              memory = "256Mi"
+            }
+            limits = {
+              cpu    = "500m"
+              memory = "500Mi"
+            }
+          }
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "rts_game_client" {
+  metadata {
+    name      = "rts-game-client"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "rts-game-client"
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+
+resource "kubernetes_deployment" "rts_game_server" {
+  metadata {
+    name      = "rts-game-server"
+    namespace = "default"
+    labels = {
+      app = "rts-game-server"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "rts-game-server"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "rts-game-server"
+        }
+      }
+
+      spec {
+        container {
+          name  = "rts-game-server"
+          image = "${aws_ecr_repository.repositories["rts-game-server-ecr"].repository_url}:latest"
+
+          env {
+            name  = "MONGO_URI"
+            value = var.mongobd-uri-value
+          }
+
+          resources {
+            requests = {
+              cpu    = "1"
+              memory = "500Mi"
+            }
+            limits = {
+              cpu    = "2"
+              memory = "1Gi"
+            }
+          }
+
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "rts_game_server" {
+  metadata {
+    name      = "rts-game-server"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "rts-game-server"
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 8080
+      target_port = 8080
+    }
+
+    type = "ClusterIP"
+  }
+}
