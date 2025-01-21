ae9345aafa03139f4582c8566e9b3356a47eb161
mmarci96 <sarosdimarci@gmail.com>
2025-01-21 09:34:18 +0100

    created networking modules to separate code

---
+++ terraform-configs/networking.tf
@@ -0,0 +1,41 @@
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.17.0"
+
+  name = "rts-game-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = var.azs
+  private_subnets = var.private_subnet_cidrs
+  public_subnets  = var.public_subnet_cidrs
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+  map_public_ip_on_launch = true
+}
+
+locals {
+  vpc_id              = module.vpc.vpc_id
+  vpc_cidr            = module.vpc.vpc_cidr_block
+  public_subnets_ids  = module.vpc.public_subnets
+  private_subnets_ids = module.vpc.private_subnets
+  subnets_ids         = concat(local.public_subnets_ids, local.private_subnets_ids)
+}
+
+
