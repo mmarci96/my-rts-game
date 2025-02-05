87457569accdd419d301d418adbd2c390abb9b76
mmarci96 <sarosdimarci@gmail.com>
2025-02-05 23:25:54 +0100

    optionals should be fine and added mongo-uri

--- terraform-configs/variables.tf
+++ terraform-configs/variables.tf
@@ -2,26 +2,36 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "eu-north-1"
 }
 
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "Private Subnet CIDR values"
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "azs" {
   type        = list(string)
   description = "Availability Zones"
+  default     = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
 }
 
 variable "ecr_repositories" {
   type        = map(string)
   description = "Map of repository names and their configurations"
+  default = {
+    "client"        = "client-repository"
+    "flask-app"     = "flask-app-repository"
+    "game-server"   = "game-server-repository"
+    "mongo-db"      = "mongo-db-repository"
+  }
 }
 
 variable "docker_images" {
@@ -30,15 +40,33 @@ variable "docker_images" {
     directory       = string
     repository_name = string
   }))
+  default = {
+    "client" = {
+      directory       = "client"
+      repository_name = "client-repository"
+    },
+    "flask-app" = {
+      directory       = "flask-app"
+      repository_name = "flask-app-repository"
+    },
+    "game-server" = {
+      directory       = "game-server"
+      repository_name = "game-server-repository"
+    },
+    "mongo-db-local" = {
+      directory       = "mongo-db-local"
+      repository_name = "mongo-db-repository"
+    }
+  }
 }
+
 variable "mongodb-uri-value" {
-  description = "Connection uri to mongodb"
+  description = "Connection URI to MongoDB"
   type        = string
 }
 
 variable "secret-key-value" {
-  description = "key encryption"
+  description = "Key encryption"
   type        = string
-
 }
 
